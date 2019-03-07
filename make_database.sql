ac8489e3a444cb43fdd120795b3f4cd462c394a6
Alexey Palshin <mykorespondencia@gmail.com>
2019-03-07 03:19:19 +0300

    sql alter rules

---
+++ make_database.sql
@@ -0,0 +1,30 @@
+
+
+-- Create database
+
+CREATE DATABASE wrenTest;
+
+-- and use...
+
+USE wrenTest;
+
+-- Create table for data
+
+    CREATE TABLE tblProductData (
+      intProductDataId int(10) unsigned NOT NULL AUTO_INCREMENT,
+      strProductName varchar(50) NOT NULL,
+      strProductDesc varchar(255) NOT NULL,
+      strProductCode varchar(10) NOT NULL,
+      dtmAdded datetime DEFAULT NULL,
+      dtmDiscontinued datetime DEFAULT NULL,
+      stmTimestamp timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+      PRIMARY KEY (intProductDataId),
+      UNIQUE KEY (strProductCode)
+    ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Stores product data';
+    
+ALTER TABLE tblProductData ADD strProductCost varchar(10) NULL;
+ALTER TABLE tblProductData ADD strProductStock varchar(10) NULL;
+ALTER TABLE tblProductData
+  MODIFY COLUMN strProductStock varchar(10) AFTER strProductCode,
+  MODIFY COLUMN strProductCost varchar(10) AFTER strProductStock;
+
